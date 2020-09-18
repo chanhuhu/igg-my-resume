cd17f0f98a6469cc045dcc58cf8ab0f2921b85f3
Aphimuk Yimsukpaitoon <luroman6@gmail.com>
2020-09-18 11:35:41 +0700

    removed mock up navigation between home and details and added resumeform validator

--- App.js
+++ App.js
@@ -1,35 +1,16 @@
 import * as React from 'react';
-import { View, Text, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-
-function HomeScreen({navigation}) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-      <Button title="Go to details" onPress={() => navigation.navigate('Details')} />
-    </View>
-  );
-}
-
-function DetailsScreen({navigation}) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Detail Screen</Text>
-    <Button title="Go to home" onPress={() => navigation.navigate('Home')} />
-    </View>
-  );
-}
+import ResumeFormScreen from './screen/ResumeForm'
 
 const Stack = createStackNavigator();
 
 function App() {
   return (
     <NavigationContainer>
-    <Stack.Navigator>
-    <Stack.Screen name="Home" title="Home" component={HomeScreen} />
-    <Stack.Screen name="Details" title="Details" component={DetailsScreen} />
-    </Stack.Navigator>
+      <Stack.Navigator>
+        <Stack.Screen name="ResumeForm" options={{title:"Resume Form"}} component={ResumeFormScreen} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
